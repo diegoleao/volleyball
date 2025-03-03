5dbd9e6dfbc66b326597ea3768b60c1b9200a721
DiegoLeao <diegoleao@gmail.com>
2025-03-03 17:20:57 -0300

    Simplificado jumpcomponent de volta

--- Assets/Scripts/Networking/Characters/JumpComponent.cs
+++ Assets/Scripts/Networking/Characters/JumpComponent.cs
@@ -6,7 +6,6 @@ public class JumpComponent : NetworkBehaviour
     public bool IsLocalPlayer;
     [SerializeField] private float jumpForce = 10f;
     private NetworkCharacterController netCharController;
-    private CharacterController charController;
 
     void Awake()
     {
@@ -17,26 +16,13 @@ public class JumpComponent : NetworkBehaviour
     public void Jump()
     {
         Debug.Log("[Player] Jump pressed");
-        if (IsGrounded())
+        if (netCharController.Grounded)
         {
             Vector3 velocity = netCharController.Velocity;
             velocity.y = jumpForce;
             netCharController.Velocity = velocity;
             Debug.Log("[Player] Player jumped!");
 
-        }
-
-    }
-
-    private bool IsGrounded()
-    {
-        if (IsLocalPlayer)
-        {
-            return charController.isGrounded;
-        }
-        else
-        {
-            return netCharController.Grounded;
         }
 
     }
